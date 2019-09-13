cf3bf31051644f869ddfed0ee4fa936492f622a9
Richard Kosegi <richard.kosegi@gmail.com>
2019-09-13 07:52:25 +0200

    Enhance error message when mapping for shard does not exists
    Change-Id: I3d35a6d79241bc83cced46dc97ea22eb87ccf222
    Signed-off-by: Richard Kosegi <richard.kosegi@gmail.com>

--- impl/src/main/java/org/opendaylight/jsonrpc/impl/RemoteShardAware.java
+++ impl/src/main/java/org/opendaylight/jsonrpc/impl/RemoteShardAware.java
@@ -12,7 +12,7 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.gson.JsonElement;
 
-import java.util.NoSuchElementException;
+import java.util.function.Supplier;
 
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.jsonrpc.bus.messagelib.TransportFactory;
@@ -29,6 +29,9 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
  * @since May 6, 2018
  */
 abstract class RemoteShardAware extends AbstractJsonRPCComponent implements AutoCloseable {
+    private static final String SHARD_NOT_AVAILABLE = "Datastore mapping does not exists "
+            + "for store '%s'at path '%s'.Make sure that requested path is within configured data endpoints "
+            + "or governance is aware of such path.";
     private final LoadingCache<String, RemoteOmShard> shardCache = CacheBuilder.newBuilder()
             .build(new CacheLoader<String, RemoteOmShard>() {
                 @Override
@@ -48,7 +51,19 @@ abstract class RemoteShardAware extends AbstractJsonRPCComponent implements Auto
     }
 
     protected String lookupEndPoint(final LogicalDatastoreType store, final JsonElement path) {
-        return pathMap.lookup(path, DataType.forDatastore(store)).orElseThrow(NoSuchElementException::new);
+        return pathMap.lookup(path, DataType.forDatastore(store)).orElseThrow(shardNotAvailable(store, path));
+    }
+
+    /**
+     * Provide human readable error when mapping for requested datastore/path does not exists.
+     *
+     * @param store datastore that is subject to current data operation
+     * @param path that is subject to current data operation
+     * @return {@link Supplier} that provides {@link IllegalArgumentException}
+     */
+    private Supplier<IllegalArgumentException> shardNotAvailable(final LogicalDatastoreType store,
+            final JsonElement path) {
+        return () -> new IllegalArgumentException(String.format(SHARD_NOT_AVAILABLE, store, path));
     }
 
     @Override
