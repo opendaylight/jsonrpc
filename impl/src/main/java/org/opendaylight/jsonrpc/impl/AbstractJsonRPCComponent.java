19b12c2c6e5eec742c2102ea71d196032e54a539
Richard Kosegi <richard.kosegi@gmail.com>
2020-02-15 06:29:27 +0100

    Relax check for known endpoint in RPC bridge
    RPC bridge is checking for known endpoint during mount, which is
    not happening in Notification component. Also behavior in global
    schema is not strict - you can have YANG model without actual RPC
    implementation.
    RPC bridge is checking for known endpoint again at invocation time anyway.
    
    Unify behavior between RPC and notifications.
    While at it, there is hunk of duplicate code which can be easily
    eliminated by moving common parts into AbstractJsonRpcComponent
    
    Change-Id: Ie542df3fed8d75572a592ad440d3cf41b4b91898
    Signed-off-by: Richard Kosegi <richard.kosegi@gmail.com>

--- impl/src/main/java/org/opendaylight/jsonrpc/impl/AbstractJsonRPCComponent.java
+++ impl/src/main/java/org/opendaylight/jsonrpc/impl/AbstractJsonRPCComponent.java
@@ -8,15 +8,22 @@
 package org.opendaylight.jsonrpc.impl;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Objects;
 
 import org.eclipse.jdt.annotation.NonNull;
+
 import org.opendaylight.jsonrpc.bus.messagelib.TransportFactory;
 import org.opendaylight.jsonrpc.hmap.DataType;
 import org.opendaylight.jsonrpc.hmap.HierarchicalEnumMap;
+import org.opendaylight.jsonrpc.model.RemoteGovernance;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.jsonrpc.rev161201.Peer;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Abstract class containing common fields for all JSONRPC components (Data
@@ -26,6 +33,7 @@ import org.opendaylight.yangtools.yang.model.api.SchemaContext;
  * @since Apr 21, 2018
  */
 abstract class AbstractJsonRPCComponent {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractJsonRPCComponent.class);
     protected final SchemaContext schemaContext;
     protected final JsonConverter jsonConverter;
     protected final TransportFactory transportFactory;
@@ -40,5 +48,25 @@ abstract class AbstractJsonRPCComponent {
         this.pathMap = Objects.requireNonNull(pathMap);
         this.jsonConverter = Objects.requireNonNull(jsonConverter);
         this.peer = Objects.requireNonNull(peer);
+    }
+
+    protected JsonObject createRootPath(Module module, QName node) {
+        final String topLevel = jsonConverter.makeQualifiedName(module, node);
+        final JsonObject path = new JsonObject();
+        path.add(topLevel, new JsonObject());
+        return path;
+    }
+
+    protected String getEndpoint(final DataType type, final RemoteGovernance governance, final JsonObject path) {
+        String endpoint = pathMap.lookup(path, type).orElse(null);
+        LOG.debug("[{}][{}] map lookup => {}", peer.getName(), type, endpoint);
+        if (endpoint == null && governance != null) {
+            endpoint = governance.governance(-1, peer.getName(), path);
+            LOG.debug("[{}][{}] governance lookup => {}", peer.getName(), type, endpoint);
+            if (endpoint != null) {
+                pathMap.put(path, type, endpoint);
+            }
+        }
+        return endpoint;
     }
 }
