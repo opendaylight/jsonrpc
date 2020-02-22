8f5cb677fb6af3b5ced6eb3223112da3aff099c1
Richard Kosegi <richard.kosegi@gmail.com>
2020-02-22 15:16:50 +0100

    Add simple adapter for JsonReader
    There are multiple places across impl module where JsonParserStream
    require instance of JsonReader, but always there is JsonElement.
    Add simple wrapper that creates instance of JsonReader from JsonElement
    
    Change-Id: I2532ab1ad2097d148083b21a2c215045d523c89b
    Signed-off-by: Richard Kosegi <richard.kosegi@gmail.com>

---
+++ impl/src/main/java/org/opendaylight/jsonrpc/model/JsonReaderAdapter.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 Lumina Networks, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.jsonrpc.model;
+
+import com.google.gson.JsonElement;
+import com.google.gson.stream.JsonReader;
+import java.io.Reader;
+import java.io.StringReader;
+import org.eclipse.jdt.annotation.NonNull;
+
+/**
+ * Helper class to eliminate repeated <code>new JsonReader(new StringReader(json.toString()))</code>.
+ *
+ * @author <a href="mailto:richard.kosegi@gmail.com">Richard Kosegi</a>
+ * @since Feb 22, 2020
+ */
+public final class JsonReaderAdapter extends JsonReader {
+    private JsonReaderAdapter(Reader in) {
+        super(in);
+    }
+
+    /**
+     * Create new instance of {@link JsonReader} using provided {@link JsonElement}.
+     *
+     * @param json {@link JsonElement} to create reader from.
+     * @return new {@link JsonReader} instance.
+     */
+    public static JsonReader from(@NonNull JsonElement json) {
+        return new JsonReaderAdapter(new StringReader(json.toString()));
+    }
+}
