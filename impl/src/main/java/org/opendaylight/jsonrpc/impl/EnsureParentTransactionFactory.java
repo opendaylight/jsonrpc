8513120b4ed172fdf892f7c07aa25a4175e3cf79
Richard Kosegi <richard.kosegi@gmail.com>
2020-03-02 19:51:01 +0100

    Move datastore related methods into separate class
    Datastore operations in RemoteControl can be reused in standalone applications,
    so it make sense to move them into separate class.
    
    Change-Id: Ifd9a1355688f142eae6a47bff593b662117a5f0e
    Signed-off-by: Richard Kosegi <richard.kosegi@gmail.com>
    (cherry picked from commit 56a832585743b831de0344cca4a4edb59e90a5a6)

--- impl/src/main/java/org/opendaylight/jsonrpc/impl/EnsureParentTransactionFactory.java
+++ impl/src/main/java/org/opendaylight/jsonrpc/impl/EnsureParentTransactionFactory.java
@@ -7,19 +7,24 @@
  */
 package org.opendaylight.jsonrpc.impl;
 
+import com.google.common.base.Preconditions;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.jsonrpc.model.TransactionFactory;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.mdsal.dom.api.DOMDataBroker;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeWriteTransaction;
 import org.opendaylight.mdsal.dom.spi.ForwardingDOMDataWriteTransaction;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 /**
- * Implementation of {@link TransactionFactory} which follows semantics of
- * {@link AbstractWriteTransaction}.
+ * Implementation of {@link TransactionFactory} which follows semantics of {@link AbstractWriteTransaction}.
  *
  * @author <a href="mailto:rkosegi@brocade.com">Richard Kosegi</a>
  *
@@ -39,16 +44,29 @@ class EnsureParentTransactionFactory implements TransactionFactory {
         return new ForwardingDOMDataWriteTransaction() {
             @Override
             public void merge(LogicalDatastoreType store, YangInstanceIdentifier path, NormalizedNode<?, ?> data) {
-                final YangInstanceIdentifier parentPath = path.getParent();
-                if (parentPath != null) {
-                    final NormalizedNode<?, ?> parentNode = ImmutableNodes.fromInstanceId(schemaContext, parentPath);
-                    delegate().merge(store, YangInstanceIdentifier.create(parentNode.getIdentifier()), parentNode);
+                final List<PathArgument> normalizedPathWithoutChildArgs = new ArrayList<>();
+                YangInstanceIdentifier rootNormalizedPath = null;
+                final Iterator<PathArgument> it = path.getPathArguments().iterator();
+                while (it.hasNext()) {
+                    final PathArgument pathArgument = it.next();
+                    if (rootNormalizedPath == null) {
+                        rootNormalizedPath = YangInstanceIdentifier.create(pathArgument);
+                    }
+                    if (it.hasNext()) {
+                        normalizedPathWithoutChildArgs.add(pathArgument);
+                    }
                 }
-                super.merge(store, path, data);
+                if (normalizedPathWithoutChildArgs.isEmpty()) {
+                    return;
+                }
+                Preconditions.checkArgument(rootNormalizedPath != null, "Empty path received");
+                final NormalizedNode<?, ?> parentStructure = ImmutableNodes.fromInstanceId(schemaContext,
+                        YangInstanceIdentifier.create(normalizedPathWithoutChildArgs));
+                delegate().merge(store, rootNormalizedPath, parentStructure);
             }
 
             @Override
-            protected DOMDataTreeWriteTransaction delegate() {
+            protected @NonNull DOMDataTreeWriteTransaction delegate() {
                 return delegateTx;
             }
         };
