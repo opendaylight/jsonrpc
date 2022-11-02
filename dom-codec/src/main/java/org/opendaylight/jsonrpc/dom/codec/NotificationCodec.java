871c6db8abf826203fef0d965ea08fa42f60a7e9
Robert Varga <robert.varga@pantheon.tech>
2022-11-02 21:18:54 +0100

    Do not reference builder implementations
    Builders should be instantiated through Builders static factory methods,
    not directly.
    
    Change-Id: Ib5793bf46a57062018bf4e6b2f593af598c960c8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- dom-codec/src/main/java/org/opendaylight/jsonrpc/dom/codec/NotificationCodec.java
+++ dom-codec/src/main/java/org/opendaylight/jsonrpc/dom/codec/NotificationCodec.java
@@ -20,7 +20,7 @@ import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.builder.DataContainerNodeBuilder;
 import org.opendaylight.yangtools.yang.data.api.schema.stream.NormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.codec.gson.JsonParserStream;
-import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.api.NotificationDefinition;
 import org.opendaylight.yangtools.yang.model.api.stmt.SchemaNodeIdentifier.Absolute;
@@ -82,7 +82,7 @@ class NotificationCodec extends RpcNotificationBaseCodec<DOMNotification> {
         }
 
         JsonRpcNotification(final Absolute schemaPath) {
-            this(ImmutableContainerNodeBuilder.create().build(), schemaPath);
+            this(Builders.containerBuilder().build(), schemaPath);
         }
 
         JsonRpcNotification(final ContainerNode content, final Absolute schemaPath) {
