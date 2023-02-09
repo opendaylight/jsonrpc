3e966c0ef9b65599e1df5f032737ef1195ef5558
Robert Varga <robert.varga@pantheon.tech>
2023-02-09 23:14:44 +0100

    Convert bus/transport-http to OSGi DS
    The blueprint here is extremely mechanic, replace it with
    equally-mechanic Declarative Services.
    
    Change-Id: I5b54d867d4bbed0633a1c21e693dcc53e254aa96
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bus/transport-http/src/main/java/org/opendaylight/jsonrpc/bus/http/HttpsBusSessionFactory.java
+++ bus/transport-http/src/main/java/org/opendaylight/jsonrpc/bus/http/HttpsBusSessionFactory.java
@@ -12,6 +12,10 @@ import org.opendaylight.jsonrpc.bus.api.BusSessionFactory;
 import org.opendaylight.jsonrpc.bus.spi.EventLoopConfiguration;
 import org.opendaylight.jsonrpc.security.api.SecurityService;
 import org.opendaylight.jsonrpc.security.noop.NoopSecurityService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * {@link BusSessionFactory} implemented using secured HTTP/1.1 channel.
@@ -20,16 +24,24 @@ import org.opendaylight.jsonrpc.security.noop.NoopSecurityService;
  * @since Mar 9, 2018
  */
 @MetaInfServices(value = BusSessionFactory.class)
+@Component(service = BusSessionFactory.class, property = "scheme=https")
 public class HttpsBusSessionFactory extends AbstractWebBusSessionFactory {
     public HttpsBusSessionFactory() {
         super("https", true, false, 443);
     }
 
-    public HttpsBusSessionFactory(final EventLoopConfiguration config, final SecurityService securityService) {
+    @Activate
+    public HttpsBusSessionFactory(@Reference(target = "(name=jsonrpc)") EventLoopConfiguration config,
+            @Reference SecurityService securityService) {
         super("https", true, false, 443, config, securityService);
     }
 
     public HttpsBusSessionFactory(final EventLoopConfiguration config) {
         this(config, NoopSecurityService.INSTANCE);
+    }
+
+    @Deactivate
+    public void deactivate() {
+        close();
     }
 }
