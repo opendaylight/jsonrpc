dd24d07bd5457760327884057c1717d490171edb
Robert Varga <robert.varga@pantheon.tech>
2024-01-29 12:02:01 +0100

    Bump upstreams
    Adopt:
    - yangtools-13.0.1
    - mdsal-13.0.0
    - controller-9.0.0
    - aaa-0.19.1
    - netconf-7.0.0
    
    Change-Id: I85c3db7ef3d85e3e0cb6ff3b9efd795ecb34c72a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- provider/common/src/test/java/org/opendaylight/jsonrpc/provider/common/RemoteRpcInvokerTest.java
+++ provider/common/src/test/java/org/opendaylight/jsonrpc/provider/common/RemoteRpcInvokerTest.java
@@ -9,6 +9,7 @@ package org.opendaylight.jsonrpc.provider.common;
 
 import static org.junit.Assert.assertNotNull;
 
+import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.junit.After;
@@ -18,12 +19,21 @@ import org.opendaylight.jsonrpc.bus.messagelib.DefaultTransportFactory;
 import org.opendaylight.jsonrpc.dom.codec.JsonRpcCodecFactory;
 import org.opendaylight.jsonrpc.impl.RemoteControl;
 import org.opendaylight.jsonrpc.model.RemoteRpcInvoker;
-import org.opendaylight.jsonrpc.test.TestModelServiceImpl;
+import org.opendaylight.jsonrpc.test.TestErrorMethod;
+import org.opendaylight.jsonrpc.test.TestFactorial;
+import org.opendaylight.jsonrpc.test.TestMultiplyList;
+import org.opendaylight.jsonrpc.test.TestRemoveCoffeePot;
+import org.opendaylight.jsonrpc.test.TestSimpleMethod;
 import org.opendaylight.mdsal.binding.dom.adapter.BindingDOMRpcProviderServiceAdapter;
 import org.opendaylight.mdsal.binding.dom.adapter.ConstantAdapterContext;
 import org.opendaylight.mdsal.binding.dom.codec.impl.BindingCodecContext;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.jsonrpc.test.rpc.rev201014.TestModelRpcService;
-import org.opendaylight.yangtools.concepts.ObjectRegistration;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.jsonrpc.test.rpc.rev201014.ErrorMethod;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.jsonrpc.test.rpc.rev201014.Factorial;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.jsonrpc.test.rpc.rev201014.MultiplyList;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.jsonrpc.test.rpc.rev201014.RemoveCoffeePot;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.jsonrpc.test.rpc.rev201014.SimpleMethod;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.binding.Rpc;
 
 /**
  * Test for {@link RemoteRpcInvoker} part of {@link RemoteControl}.
@@ -33,7 +43,7 @@ import org.opendaylight.yangtools.concepts.ObjectRegistration;
  */
 public class RemoteRpcInvokerTest extends AbstractJsonRpcTest {
     private DefaultTransportFactory transportFactory;
-    private ObjectRegistration<TestModelServiceImpl> rpcReg;
+    private Registration rpcReg;
     private RemoteControl ctrl;
     private JsonRpcCodecFactory codecFactory;
 
@@ -41,13 +51,19 @@ public class RemoteRpcInvokerTest extends AbstractJsonRpcTest {
     public void setUp() throws Exception {
         final BindingDOMRpcProviderServiceAdapter rpcAdapter = new BindingDOMRpcProviderServiceAdapter(
                 new ConstantAdapterContext(new BindingCodecContext(getBindingRuntimeContext())),
-                getDOMRpcRouter().getRpcProviderService());
-        rpcReg = rpcAdapter.registerRpcImplementation(TestModelRpcService.class, new TestModelServiceImpl());
-        getDOMRpcRouter().onModelContextUpdated(schemaContext);
+                getDOMRpcRouter().rpcProviderService());
+        rpcReg = rpcAdapter.registerRpcImplementations(ImmutableClassToInstanceMap.<Rpc<?, ?>>builder()
+            .put(ErrorMethod.class, new TestErrorMethod())
+            .put(Factorial.class, new TestFactorial())
+            .put(MultiplyList.class, new TestMultiplyList())
+            .put(RemoveCoffeePot.class, new TestRemoveCoffeePot())
+            .put(SimpleMethod.class, new TestSimpleMethod())
+            .build());
+//        getDOMRpcRouter().onModelContextUpdated(schemaContext);
         codecFactory = new JsonRpcCodecFactory(schemaContext);
         transportFactory = new DefaultTransportFactory();
-        ctrl = new RemoteControl(getDomBroker(), schemaContext, transportFactory, getDOMNotificationRouter(),
-                getDOMRpcRouter().getRpcService(), codecFactory);
+        ctrl = new RemoteControl(getDomBroker(), schemaContext, transportFactory,
+            getDOMNotificationRouter().notificationPublishService(), getDOMRpcRouter().rpcService(), codecFactory);
 
         logTestName("START");
     }
