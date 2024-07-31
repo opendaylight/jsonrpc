feae2f5b524a5374b5ef92f53c01ee7205b94b54
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-07-31 13:30:03 +0200

    Bump upstreams
    Adopt:
    - odlparent-14.1.0
    - infrautils-7.1.4
    - yangtools-14.0.14
    - mdsal-14.0.13
    - controller-11.0.0
    - aaa-0.21.0
    - netconf-9.0.0
    
    This patch also updates dependencies to reflect netty-4.2.
    
    We also adapt to the switch from Akka to Apache Pekko, as needed
    for controller-11.0.0 as per CONTROLLER-2120:
    - changing dependencies
    - changing the imports
    - adapting configuration
    
    JIRA: JSONRPC-60
    Change-Id: I8c29e0dc76553c3bf13482dc99203c544b90b494
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bus/transport-zmq/src/test/java/org/opendaylight/jsonrpc/bus/zmq/Zmtp20NegotiationTest.java
+++ bus/transport-zmq/src/test/java/org/opendaylight/jsonrpc/bus/zmq/Zmtp20NegotiationTest.java
@@ -14,7 +14,8 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.MultiThreadIoEventLoopGroup;
+import io.netty.channel.nio.NioIoHandler;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -47,7 +48,7 @@ public class Zmtp20NegotiationTest implements MessageListener {
 
     @Before
     public void setUp() {
-        group = new NioEventLoopGroup(4);
+        group = new MultiThreadIoEventLoopGroup(4, NioIoHandler.newFactory());
         sb = new ServerBootstrap().group(group).childHandler(new ChannelInitializer<SocketChannel>() {
             @Override
             protected void initChannel(SocketChannel ch) throws Exception {
