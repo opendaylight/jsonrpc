9653421834534eace16bba93babf4ffd835965c1
Robert Varga <robert.varga@pantheon.tech>
2025-09-29 13:11:15 +0200

    WIP: Bump upstreams
    Adopt:
    - ietf-1.0.0
    - mdsal-15.0.0
    - controller-12.0.0
    - aaa-0.22.0
    - netconf-10.0.0-SNAPSHOT
    
    Change-Id: Ic7e5df3d32709e7ceb66ba15f0c8d0a9f9c92875
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- provider/cluster/src/test/java/org/opendaylight/jsonrpc/provider/cluster/TestCustomizer.java
+++ provider/cluster/src/test/java/org/opendaylight/jsonrpc/provider/cluster/TestCustomizer.java
@@ -15,14 +15,22 @@ import org.opendaylight.mdsal.dom.broker.DOMMountPointServiceImpl;
 import org.opendaylight.mdsal.dom.broker.DOMNotificationRouter;
 import org.opendaylight.mdsal.dom.broker.DOMRpcRouter;
 import org.opendaylight.mdsal.dom.broker.RouterDOMPublishNotificationService;
+import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 
 public class TestCustomizer extends AbstractConcurrentDataBrokerTest {
     private final DOMMountPointServiceImpl mountPointService = new DOMMountPointServiceImpl();
-    private final DOMRpcRouter rpcRouter = new DOMRpcRouter();
     private final DOMNotificationRouter notificationRouter = new DOMNotificationRouter(1);
+
+    private DOMRpcRouter rpcRouter;
 
     public TestCustomizer() {
         super(true);
+    }
+
+    @Override
+    protected void setupWithSchema(final EffectiveModelContext context) {
+        super.setupWithSchema(context);
+        rpcRouter = new DOMRpcRouter(getSchemaService());
     }
 
     public DOMMountPointService getDOMMountPointService() {
